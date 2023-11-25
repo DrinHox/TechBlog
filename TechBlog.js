63bc7f37a2b9908151248fa18b4b40835ba669f7
DrinHox <93438101+DrinHox@users.noreply.github.com>
2023-11-25 20:53:52 +0100

    Slider
    added the slider and made some adjustments

--- TechBlog.js
+++ TechBlog.js
@@ -19,3 +19,23 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const slider = document.querySelector('.slider');
+    
+    let count = 0;
+
+    function nextSlide() {
+        count++;
+        if (count >= slider.children.length) {
+            count = 0;
+        }
+        updateSlider();
+    }
+
+    function updateSlider() {
+        slider.style.transform = `translateX(${-count * 100}%)`;
+    }
+
+    setInterval(nextSlide, 3000);
+});
