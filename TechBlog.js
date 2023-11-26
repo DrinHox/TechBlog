d4f3f6b3353c399a00c43344640d659c2c21e594
fuadiubt <fs52850@ubt-uni.net>
2023-11-26 01:26:09 +0100

    slider changes

--- TechBlog.js
+++ TechBlog.js
@@ -22,20 +22,47 @@ document.addEventListener('DOMContentLoaded', function() {
 
 document.addEventListener('DOMContentLoaded', function() {
     const slider = document.querySelector('.slider');
-    
+    const dotsContainer = document.querySelector('.dots');
+
     let count = 0;
 
     function nextSlide() {
         count++;
-        if (count >= slider.children.length) {
-            count = 0;
+        if (count >= slider.children.length -1) {
+            count = 1;
+        }
+        updateSlider();
+    }
+
+    function previousSlide() {
+        count--;
+        if (count < 0) {
+            count = slider.children.length - 2;
         }
         updateSlider();
     }
 
     function updateSlider() {
         slider.style.transform = `translateX(${-count * 100}%)`;
+
+        // Update active dot
+        document.querySelectorAll('.dot').forEach(dot => dot.classList.remove('active'));
+        document.querySelectorAll('.dot')[count].classList.add('active');
     }
+
+    // Create dots and slide buttons for each slide
+    for (let i = 0; i < slider.children.length; i++) {
+        const dot = document.createElement('span');
+        dot.classList.add('dot');
+        dotsContainer.appendChild(dot);
+
+        dot.addEventListener('click', () => {
+            count = i;
+            updateSlider();
+        });
+    }
+
+    updateSlider(); // Initialize the slider with the first slide
 
     setInterval(nextSlide, 3000);
 });
