44a82f396e9e787f706eed55430174819e73722f
DrinHox <93438101+DrinHox@users.noreply.github.com>
2023-11-27 01:38:44 +0100

    Modal Form
    Modal form for sing in and register

--- TechBlog.js
+++ TechBlog.js
@@ -1,22 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var signInButton = document.getElementById('signin-btn');
-
-    if (signInButton) {
-        signInButton.addEventListener('click', function() {
-            
-            var username = prompt('Enter your username:');
-            var password = prompt('Enter your password:');
-
-            
-            if (username && password) {
-                
-                alert('Successful Sign In!\nUsername: ' + username);
-            } else {
-                alert('Sign In canceled or invalid input.');
-            }
-        });
-    }
-
+document.addEventListener('DOMContentLoaded', function () {
 
     const slider = document.querySelector('.slider');
     const dotsContainer = document.querySelector('.dots');
@@ -25,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     function nextSlide() {
         count++;
-        if (count >= slider.children.length -1) {
+        if (count >= slider.children.length - 1) {
             count = 0;
         }
         updateSlider();
@@ -80,7 +62,29 @@ document.addEventListener('DOMContentLoaded', function() {
 
 });
 
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function () {
     const scrollPosition = window.pageYOffset;
     document.body.style.backgroundPositionY = -scrollPosition * 0.3 + 'px';
 });
+
+function openModal(modalId) {
+    const modal = document.getElementById(modalId);
+    modal.style.display = 'block';
+}
+
+
+function closeModal(modalId) {
+    const modal = document.getElementById(modalId);
+    modal.style.display = 'none';
+}
+
+
+window.onclick = function (event) {
+    const modals = document.getElementsByClassName('modal');
+    for (const modal of modals) {
+        if (event.target === modal) {
+            modal.style.display = 'none';
+        }
+    }
+};
+
